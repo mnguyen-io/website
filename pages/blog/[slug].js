33a8be6c5e894eb55a700f8eadc7a36c8dc9a528
Carey Janecka <careyjanecka@gmail.com>
2020-08-25 14:02:58 -0700

    Fix build

--- pages/blog/[slug].js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:407135e3ee4366080fa8f3a2a549ee77699b9e19c56ef9190ff31da41257cb16
-size 56
--- pages/blog/index.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:856ceac73d3637878cbacee4b8e9a83d1efe92c0d5568de0cebfd43efe2b7671
-size 51
